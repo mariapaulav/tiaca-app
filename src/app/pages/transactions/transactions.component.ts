2e99a3d08ca0e5929d11cab00e3644b9bdba882e
mariapaulav <mariapaulaviviz@gmail.com>
2023-07-06 07:19:18 -0500

    final retouches :sparkles:

--- src/app/pages/transactions/transactions.component.ts
+++ src/app/pages/transactions/transactions.component.ts
@@ -8,16 +8,33 @@ import { Invoice } from 'src/app/models/invoice.models';
   styleUrls: ['./transactions.component.css']
 })
 export class TransactionsComponent {
-http=inject(HttpClient)
-invoices:Invoice[]=[]
+  http=inject(HttpClient)
+  invoices:Invoice[]=[]
 
-ngOnInit(){
-  this.http.get<Invoice[]>('../../../assets/data.json')
-    .subscribe(data => {
-    this.invoices = data;
-    console.log(data)
-    });
-  }
+  intervalId: any;
+  modalOpen = false;
+
+  ngOnInit(){
+    this.http.get<Invoice[]>('../../../assets/data.json')
+      .subscribe(data => {
+      this.invoices = data;
+      });
+      this.intervalId = setInterval(() => {
+      this.closeModal();
+    }, 2000);
+  };
+
+  openModal() {
+    this.modalOpen = true;
+  };
+
+  closeModal() {
+    this.modalOpen = false;
+  };
+  
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  };
 }
 
 
